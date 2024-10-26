ee0a4f457c80622f9d792b6df843ae598e9d8f45
viys <2628587386@qq.com>
2024-10-26 14:27:50 +0800

    Generate helper functions quickly

--- CuTest.h
+++ CuTest.h
@@ -5,7 +5,32 @@
 #include <stdarg.h>
 #include <stddef.h>
 
-#define CUTEST_VERSION  "CuTest 1.5c"
+#define CUTEST_VERSION  "CuTest 1.5.1c"
+
+/* Helper functions */
+
+#define CREATE_ASSERTS(Asserts) \
+void Asserts(CuTest* tc, const char* message, const char* expected, CuString *actual) { \
+    int mismatch; \
+    if (expected == NULL || actual == NULL) { \
+        mismatch = (expected != NULL || actual != NULL); \
+    } else { \
+        const char *front = __FILE__ ":"; \
+        const size_t frontLen = strlen(front); \
+        const size_t expectedLen = strlen(expected); \
+        const char *matchStr = actual->buffer; \
+        mismatch = (strncmp(matchStr, front, frontLen) != 0); \
+        if (!mismatch) { \
+            matchStr = strchr(matchStr + frontLen, ':'); \
+            mismatch |= (matchStr == NULL || strncmp(matchStr, ": ", 2)); \
+            if (!mismatch) { \
+                matchStr += 2; \
+                mismatch |= (strncmp(matchStr, expected, expectedLen) != 0); \
+            } \
+        } \
+    } \
+    CuAssert_Line(tc, __FILE__, __LINE__, message, !mismatch); \
+}
 
 /* CuString */
 
